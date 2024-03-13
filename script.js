5c783ccd79385bd10222a4ca731cf4be74c74a58
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2024-03-13 06:27:30 +0300

    JavaScript'de hata durumları düzeltildi.

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-
-
 let index = document.querySelector(".clockText");
 
 let deger = setInterval(()=>{
-    setState();
-},1000);
-
-function setState()
-{
     let date = new Date();
-    let pam = date.get
-    let Hours =date.getHours();
+    let Hours = date.getHours();
     let Minute = date.getMinutes();
     let Second = date.getSeconds();
-    let Message = null;
+    let Timer = "AM";
+
+    if(Hours >= 12)
+    {
+        Hours = Hours - 12;
+        Timer = "PM";
+
+    }
+
+    if(Hours < 10)
+    {
+        Hours = "0" + Hours;
+    }
+    if(Second < 10)
+    {
+        Second = "0" + Second;
+    }
+    if(Minute < 10)
+    {
+        Minute = "0" + Minute;
+    }
     
-    if(Hours>=12)
-    {
-        Message = "PM";
-    }
-    else{
-        Message = "AM";
-    }
-
-    if(Hours<10)
-    {
-        let newHours = "0" + Hours;
-        if(Second<10)
-        {
-            let newSecond = "0" + Second;
-            index.innerHTML = `${newHours}:${Minute}:${newSecond} ${Message}`;
-        }
-        else{
-            index.innerHTML = `${newHours}:${Minute}:${Second} ${Message}`;
-        }
-    }
-
-
-
-};
+    index.innerHTML = `${Hours}:${Minute}:${Second} :${Timer}`;
+}, 1000);
